068541f9a0afe7566b9c4bcf73922798ed71e78a
shreyo121 <73705088+shreyo121@users.noreply.github.com>
2020-10-30 19:47:49 +0530

    Add files via upload

---
+++ Java Script/main.js
@@ -0,0 +1,13 @@
+console.log("My First Console Project");
+var firstname="shreyan";
+var lastname="agarwal";
+var fullname=firstname.concat(lastname); 
+console.log(fullname);
+var number1="15";
+console.log(number1);
+var number2="5";
+console.log(number2);
+var addded_number=number1+number2;
+console.log(addded_number);
+var subtracted_number=number1-number2;
+console.log(subtracted_number);
